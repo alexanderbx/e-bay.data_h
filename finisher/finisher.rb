c4f3743cbf52568c66e46d422ef7ad16fe34ee52
Alx_Bx <cristalexander@gmail.com>
2020-10-17 19:37:06 -0600

    add finisher

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -1,4 +1,4 @@
-collections = Datahen::Client::SraperJobOutput::.new.collections("ebay")
+collections = Datahen::Client::ScraperJobOutput.new.collections("ebay")
 collection = collections.find{|collection| collection['collection'] == "listings"}
 if collection 
     total = collection["outputs"]
